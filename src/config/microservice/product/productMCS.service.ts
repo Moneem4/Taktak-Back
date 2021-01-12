de28ebfbcd29572fee36f40ce053d993f0f59818
Moneem Mtiraoui <monemmtiraoui1995@gmail.com>
2021-01-12 10:36:35 +0100

    gggg

--- src/config/microservice/product/productMCS.service.ts
+++ src/config/microservice/product/productMCS.service.ts
@@ -3,14 +3,7 @@ import { ClientProxy } from '@nestjs/microservices'
 
 @Injectable()
 export class ProductMCS {
-<<<<<<< HEAD
-	constructor(
-		@Inject(' Product-SERVICE') private readonly client: ClientProxy
-	) {
-		console.log('init  Product-SERVICE')
-=======
 	constructor(@Inject('PRODUCT-SERVICE') private readonly client: ClientProxy) {
->>>>>>> 7c7d3bb85c9b9612e66b9dca906efde7be340b7c
 	}
 
 	public send(pattern: string, data: any) {
--- src/config/microservice/service/serviceMCS.service.ts
+++ src/config/microservice/service/serviceMCS.service.ts
@@ -5,13 +5,7 @@ import { ClientProxy } from '@nestjs/microservices'
 export class ServiceMCS {
 	constructor(
 		@Inject('SERVICES_SERVICE') private readonly client: ClientProxy
-<<<<<<< HEAD
-	) {
-		console.log('init SERVICE-SERVICE')
-	}
-=======
 	) {}
->>>>>>> 7c7d3bb85c9b9612e66b9dca906efde7be340b7c
 
 	public send(pattern: string, data: any) {
 		return this.client.send(pattern, data).toPromise()
