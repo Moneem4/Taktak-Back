5df5f6822461b286211bee2d12f1b283c7d143a6
Anissa <anissabenhafsia@gmail.com>
2021-01-13 13:13:54 +0100

    fix postMCS config

--- src/config/microservice/post/postMCS.module.ts
+++ src/config/microservice/post/postMCS.module.ts
@@ -10,12 +10,10 @@ import { PostMCS } from './postMCS.service'
 				name: 'POST-SERVICE',
 				transport: Transport.RMQ,
 				options: {
-
 					urls: [
 						`amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}/${process.env.RABBITMQ_MCS_POST_VHOST}`
 					],
 					queue: `${process.env.RABBITMQ_MCS_POST_QUEUE}`,
-
 					noAck: true,
 					queueOptions: {
 						durable: true
